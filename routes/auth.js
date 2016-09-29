6c692ed771caa9f44f413632558c870bd347c171
root <anupam.basak27@gmail.com>
2016-09-30 04:55:03 +0530

    Disabled DB

--- routes/auth.js
+++ routes/auth.js
@@ -13,28 +13,49 @@ router.get("/login", function(req, res, next) {
 * Authenticate User
 **/
 router.post("/login", function(req, res, next) {
-  User.findOne({
-		email: req.body.e,
-		password: req.body.p
-	}, function(err, user) {
-		if (err) {
-			next(err)
-		}
-		else {
-			if (user === null) {
-				var error = new Error("Authentication Error");
-				error.status = 550;
+  // User.findOne({
+	// 	email: req.body.e,
+	// 	password: req.body.p
+	// }, function(err, user) {
+	// 	if (err) {
+	// 		next(err)
+	// 	}
+	// 	else {
+	// 		if (user === null) {
+	// 			var error = new Error("Authentication Error");
+	// 			error.status = 550;
+	//
+	// 			next(error);
+	// 		}
+	// 		else {
+	// 			req.session.email = req.body.e;
+	// 			req.session.name = user.name;
+	// 			req.session.userid = user._id;
+	// 			res.redirect("/");
+	// 		}
+	// 	}
+	// })
 
-				next(error);
-			}
-			else {
-				req.session.email = req.body.e;
-				req.session.name = user.name;
-				req.session.userid = user._id;
-				res.redirect("/");
-			}
-		}
-	})
+	if (req.body.e == "anupam@turret.in" && req.body.p == "password") {
+		req.session.email = req.body.e;
+		req.session.name = "Anupam Basak";
+		req.session.userid = "57ed7fbf4fcbc132e36bd49e";
+
+		res.redirect("/");
+	}
+	else if (req.body.e == "probal@turret.in" && req.body.p == "password") {
+		req.session.email = req.body.e;
+		req.session.name = "Probal Basak";
+		req.session.userid = "57ed7fbf4fcbc132e36bd49f";
+
+		res.redirect("/");
+	}
+	else {
+		var error = new Error("Authentication Error");
+		error.status = 550;
+
+		next(error);
+	}
 });
 
 /**
