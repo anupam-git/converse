dd09f48d2eed6bd782c4a1e5bbff8da3878789fe
root <teamturret@turret.in>
2016-09-30 01:48:28 +0530

    Basic Static Framework

---
+++ models/User.js
@@ -0,0 +1,17 @@
+var mongoose = require("mongoose");
+
+var User = new mongoose.Schema({
+	name: {
+		type: String,
+    required: true,
+    trim: true
+	},
+	password: String,
+  email: {
+    type: String,
+    required: true,
+    trim: true
+  }
+})
+
+module.exports = mongoose.model('User', User);
---
+++ models/dbInit.js
@@ -0,0 +1,4 @@
+var mongoose = require( 'mongoose' );
+var dbURI = 'mongodb://localhost:27017/converseDB';
+
+mongoose.connect(dbURI);
