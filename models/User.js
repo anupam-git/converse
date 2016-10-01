a3391e832fe1c2b92cc421569c3cd71ee8ecafba
root <anupam.basak27@gmail.com>
2016-10-01 12:37:36 +0530

    ClearDB Mysql Bluemix Integration

--- models/User.js
+++ models/User.js
@@ -1,17 +1,36 @@
-var mongoose = require("mongoose");
+// var mongoose = require("mongoose");
+//
+// var User = new mongoose.Schema({
+// 	name: {
+// 		type: String,
+//     required: true,
+//     trim: true
+// 	},
+// 	password: String,
+//   email: {
+//     type: String,
+//     required: true,
+//     trim: true
+//   }
+// })
+//
+// module.exports = mongoose.model('User', User, "user");
 
-var User = new mongoose.Schema({
-	name: {
-		type: String,
-    required: true,
-    trim: true
-	},
-	password: String,
-  email: {
-    type: String,
-    required: true,
-    trim: true
-  }
-})
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: "us-cdbr-iron-east-04.cleardb.net",
+  user: "b0c0e0e3cbf854",
+  password: "80204659",
+  database: "ad_a78324483943ab4",
+  debug: true
+});
 
-module.exports = mongoose.model('User', User, "user");
+module.exports.authUser = function(email, password, cb) {
+	connection.query('SELECT * FROM users WHERE email="'+email+'" AND password="'+password+'"', function(err, rows, fields) {
+	  if (err) throw err;
+
+	  return cb(rows[0]);
+
+		connection.end();
+	});
+}
--- routes/auth.js
+++ routes/auth.js
@@ -13,28 +13,42 @@ router.get("/login", function(req, res, next) {
 * Authenticate User
 **/
 router.post("/login", function(req, res, next) {
-	if (req.body.e == "anupam@turret.in" && req.body.p == "password") {
-		req.session.email = req.body.e;
-		req.session.name = "Anupam Basak";
-		req.session.userid = "1";
-		req.session.lang = "es";
+	User.authUser(req.body.e, req.body.p, function(user) {
+		if (user === undefined) {
+			var error = new Error("Authentication Error");
+			error.status = 550;
 
-		res.redirect("/");
-	}
-	else if (req.body.e == "probal@turret.in" && req.body.p == "password") {
-		req.session.email = req.body.e;
-		req.session.name = "Probal Basak";
-		req.session.userid = "2";
-		req.session.lang = "en";
+			next(error);
+		}
+		else {
+			req.session.email = req.body.e;
+			req.session.name = user.name;
+			req.session.userid = user.userid;
+			req.session.lang = user.lang;
 
-		res.redirect("/");
-	}
-	else {
-		var error = new Error("Authentication Error");
-		error.status = 550;
+			res.redirect("/");
+		}
+	})
 
-		next(error);
-	}
+	// if (req.body.e == "anupam@turret.in" && req.body.p == "password") {
+	// 	req.session.email = req.body.e;
+	// 	req.session.name = "Anupam Basak";
+	// 	req.session.userid = "1";
+	// 	req.session.lang = "es";
+	//
+	// 	res.redirect("/");
+	// }
+	// else if (req.body.e == "probal@turret.in" && req.body.p == "password") {
+	// 	req.session.email = req.body.e;
+	// 	req.session.name = "Probal Basak";
+	// 	req.session.userid = "2";
+	// 	req.session.lang = "en";
+	//
+	// 	res.redirect("/");
+	// }
+	// else {
+	//
+	// }
 });
 
 /**
