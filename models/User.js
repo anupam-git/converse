48c1e70160f2a58ef79626ec1e5743dd2a6430b9
root <anupam.basak27@gmail.com>
2016-10-01 13:33:20 +0530

    Updates : ClearDB Mysql Bluemix Integration

--- models/User.js
+++ models/User.js
@@ -17,18 +17,20 @@
 // module.exports = mongoose.model('User', User, "user");
 
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-  host: "us-cdbr-iron-east-04.cleardb.net",
-  user: "b0c0e0e3cbf854",
-  password: "80204659",
-  database: "ad_a78324483943ab4",
-  debug: true
-});
 
 module.exports.authUser = function(email, password, cb) {
+	var connection = mysql.createConnection({
+	  host: "us-cdbr-iron-east-04.cleardb.net",
+	  user: "b0c0e0e3cbf854",
+	  password: "80204659",
+	  database: "ad_a78324483943ab4",
+	  debug: true
+	});
+
 	connection.query('SELECT * FROM users WHERE email="'+email+'" AND password="'+password+'"', function(err, rows, fields) {
 	  if (err) throw err;
 
 	  return cb(rows[0]);
+		
 	});
 }
